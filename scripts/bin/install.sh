da3015396f88da6785a5c40f3f39a304987c7dd5
Oleg Samoylov <pioneeru@gmail.com>
2024-11-02 23:06:56 +0100

    full install

--- scripts/bin/install.sh
+++ scripts/bin/install.sh
@@ -3,10 +3,12 @@
 . /opt/toolbox/play-instana/artefacts.env
 . /opt/toolbox/play-instana/credentials.env
 
-wget https://artifact-public.instana.io/artifactory/rel-generic-instana-virtual/infrastructure/kubectl/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
-tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
+# wget https://artifact-public.instana.io/artifactory/rel-generic-instana-virtual/infrastructure/kubectl/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
+# tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
 
 cd /opt/toolbox/play-instana/ocp
+./2-cert-manager.sh
+./3-kubectl-instana.sh
 ./4-generate_manifests.sh
 ./5-pull_datastore_charts.sh
 ./6-install.sh
