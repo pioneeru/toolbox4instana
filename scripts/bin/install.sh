be9b9cf3a6ef8b3138b1d18bffa3a1e0cc1e98bb
Oleg Samoylov <pioneeru@gmail.com>
2024-11-21 22:56:33 +0100

    plugin url added

--- scripts/bin/install.sh
+++ scripts/bin/install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/bash
 
+git -C /opt/toolbox/play-instana checkout $INSTANA_VERSION
+
 . /opt/toolbox/play-instana/artefacts.env
 . /opt/toolbox/play-instana/credentials.env
 
-# wget https://artifact-public.instana.io/artifactory/rel-generic-instana-virtual/infrastructure/kubectl/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
-# tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
+wget ${KUBECTL_INSTANA_PLUGIN_URL}/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
+tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
 
 cd /opt/toolbox/play-instana/ocp
 ./2-cert-manager.sh
--- scripts/bin/start.sh
+++ scripts/bin/start.sh
@@ -5,7 +5,7 @@ git -C /opt/toolbox/play-instana checkout $INSTANA_VERSION
 . /opt/toolbox/play-instana/artefacts.env
 . /opt/toolbox/play-instana/credentials.env
 
-wget https://artifact-public.instana.io/artifactory/rel-generic-instana-virtual/infrastructure/kubectl/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
+wget ${KUBECTL_INSTANA_PLUGIN_URL}/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
 tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
 
 while true; do sleep 30; done
