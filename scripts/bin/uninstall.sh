dda1d79ac5ff660cb8983dde1ab74af3603b8106
Oleg Samoylov <pioneeru@gmail.com>
2024-11-22 19:06:26 +0100

    changed wget to non verbose mode

--- scripts/bin/uninstall.sh
+++ scripts/bin/uninstall.sh
@@ -6,7 +6,7 @@ git -C /opt/toolbox/play-instana checkout $INSTANA_VERSION
 . /opt/toolbox/play-instana/artefacts.env
 . /opt/toolbox/play-instana/credentials.env
 
-wget ${KUBECTL_INSTANA_PLUGIN_URL}/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY}
+wget ${KUBECTL_INSTANA_PLUGIN_URL}/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY} -nv
 tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
 
 cd /opt/toolbox/play-instana/ocp
