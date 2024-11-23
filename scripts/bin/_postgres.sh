4c16eeeedf58df6fc4ff76b8560a4f632767b8fc
Oleg Samoylov <pioneeru@gmail.com>
2024-11-23 22:19:57 +0100

    postgres tool added

---
+++ scripts/bin/_postgres.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+function checkPostgres {
+
+    case "$2" in
+        fullvacuum)
+            pgsql "vacuum full;" $3
+            ;;
+        status)
+            pg pg_isready
+            ;;
+        size)
+            pgsql "\l+"
+            ;;
+        vacuum)
+            pgsql "vacuum verbose;" $3
+            ;;
+        *|help)
+            echo "Usage: swat postgres [OPTION]"
+            echo "postgres (short: pg) options:"
+            echo "   pg help                       - show this help"
+            echo "   pg fullvacuum <DB_NAME>       - locks and copies all tables of the DB_NAME to a new ones and if succeed, deletes the old tables. TAKES MORE TIME THEN VACUUM!"
+            echo "   pg size                       - list databases and size"
+            echo "   pg status                     - check if connections accepted"
+            echo "   pg vacuum <DB_NAME>           - shrink disk space for all tables in db DB_NAME"
+            exit 1
+            ;;
+    esac 
+}
+
