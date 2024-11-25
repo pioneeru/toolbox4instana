ff448f0667a3f0d4d5b65c9520913be6e948855e
Oleg Samoylov <pioneeru@gmail.com>
2024-11-25 17:36:38 +0100

    kafka topics added

---
+++ scripts/bin/_kafka.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+function checkKafka {
+
+    case "$2" in
+        list)
+            kt --list
+            ;;
+        describe)
+            TOPIC=""
+            if [[! -z $3 ]]; then
+                TOPIC="--topic $3"
+            fi
+            kt --describe $TOPIC
+            ;;
+        *|help)
+            echo "Usage: swat install [OPTION]"
+            echo "unit options:"
+            echo "   k help                 - show this help"
+            echo "   k list                 - list topics"
+            echo "   k describe [topic]     - describe topic (Default: describes all topics"
+            exit 1
+            ;;
+    esac 
+}
+
