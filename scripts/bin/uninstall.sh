2b830b408209e4bcbc91cb41c19db0d49bd17c69
Oleg Samoylov <pioneeru@gmail.com>
2024-12-14 15:36:16 +0100

    rebase added

--- scripts/bin/uninstall.sh
+++ scripts/bin/uninstall.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-git -C /opt/toolbox/play-instana pull
+git -C /opt/toolbox/play-instana pull --rebase
 git -C /opt/toolbox/play-instana checkout $INSTANA_VERSION
 
-. /opt/toolbox/play-instana/artefacts.env
 . /opt/toolbox/play-instana/credentials.env
+. /opt/toolbox/play-instana/artefacts.env
 
 wget ${KUBECTL_INSTANA_PLUGIN_URL}/${KUBECTL_INSTANA_PLUGIN} -P /tmp --user=_ --password=${DOWNLOAD_KEY} -nv
 tar xzvf /tmp/${KUBECTL_INSTANA_PLUGIN} -C /usr/local/bin/
